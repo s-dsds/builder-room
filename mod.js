e1e8a40b8b9ef29f1da5d6d36f5409bde98f8920
dsds <sylvodsds@gmail.com>
2020-11-28 03:09:22 +0100

    what did i smoke

--- mod.js
+++ mod.js
@@ -3,12 +3,18 @@ var currMod = "0";
 
 function loadMod(mod) {
     console.log("loading mod "+mod.name);
-    window.WLROOM.loadMod(makeModZip(mod.data, base_sprites));
+    if (typeof mod.zip != "undefined") {
+        mod.zip= makeModZip(mod.data, base_sprites);
+    }
+    window.WLROOM.loadMod(mod.zip);
 }
 
 function makeModZip(basemod, sprites) {
     console.log("building zip");
     var mdzip = new JSZip();
+    if (basemod.soundpack!= "undefined") {
+        basemod = JSON.stringify(basemod);
+    }
     mdzip.file('mod.json5',basemod);
 
     mdzip.file('sprites.wlsprt', sprites, {binary:true});
